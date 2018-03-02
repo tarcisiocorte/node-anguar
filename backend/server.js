a834618e6e211a4ae3a382547286bc2b7c733a1c
tarcisiocorte <tarcisiocorte@gmail.com>
2018-03-02 14:39:06 -0300

    Getting Data with Angular

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 var express = require('express');
+var cors = require('cors');
 var app = express();
 
 var posts =[
     {message: 'hello'},
     {message: 'hi'}
 ]
+
+app.use(cors())
 
 app.get('/posts', (req, res) =>{
     res.send(posts)
