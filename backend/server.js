796b4d901640741aa6c22fd0a5f3dc78f853ad76
tarcisiocorte <tarcisiocorte@gmail.com>
2018-03-06 23:51:15 -0300

    Register Endpoint with Express

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var cors = require('cors');
 var app = express();
+var bodyParser = require('body-parser');
 
 var posts =[
     {message: 'hello'},
@@ -8,9 +9,16 @@ var posts =[
 ]
 
 app.use(cors())
+app.use(bodyParser.json())
 
 app.get('/posts', (req, res) =>{
     res.send(posts)
+})
+
+app.post('/register', (req, res) =>{
+    var userData = req.body;
+    console.log(userData.email)
+    res.sendStatus(200);
 })
 
 app.listen(3000)
